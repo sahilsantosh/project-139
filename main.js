1863d8c5af011f2b2839ab3f8db9e9f200e85345
sahilsantosh <108314838+sahilsantosh@users.noreply.github.com>
2022-09-02 08:32:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,10 +8,24 @@ function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
 }
 
 function draw() {
 	game()
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.Y;
+	}
 }
 
 
